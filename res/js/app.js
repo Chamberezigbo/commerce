d1e6c572e4ee335968401ffeac1302d548b71920
chamberezigbo <chamberzigbo@gmail.com>
2021-03-03 05:31:14 +0100

    finched my cart

--- res/js/app.js
+++ res/js/app.js
@@ -1,11 +1,5 @@
- "use script"
-// cart = () => {
+//  "use script"
 
-//    let currentCart = parseInt(document.getElementById('cart').innerHTML);
-//    currentCart = currentCart+1
-//    document.getElementById('cart').innerHTML = currentCart;
-
-// }
 
 const allProducts = [] ;
 const divCart = document.getElementById('checkout');
@@ -47,31 +41,106 @@ function cart(id, name, price) {
   
       allProducts.push(items) 
       
+}
+
+function showCart() {
+
+   if (currentCart == 0) {
+      alert('cart is empty please add product to cart thank you')
+      return;
    }
 
-   function showCart() {
+   for (const product of allProducts) {
+      let tag = document.createElement("p");
+      tag.classList.add("border")
+      tag.classList.add("border-dark")
+      tag.classList.add("py-1")
+      let text = document.createTextNode(`
+         Product Name:${product.name}, Product Price:${product.price}, Product Quantity: ${product.quantity} 
+      `);
+      tag.appendChild(text);
+      var element = document.getElementById("checkoutItem");
+      element.appendChild(tag);
 
-      if (currentCart == 0) {
-         alert('cart is empty please add product to cart thank you')
-      }else{
 
-         for (let index = 0; index < allProducts.length; index++) {
-            
-            
-            totalPrice +=parseInt(allProducts[index].price);
-            totalQuantity +=parseInt(allProducts[index].quantity)
-            // console.log(totalPrice)
-            // console.log(totalQuantity)
+   }
 
-            
-         }
-         
-         divCart.classList.remove('d-none');
-         mainBody.classList.replace('mainbody','d-none')
-      }
+   for (let index = 0; index < allProducts.length; index++) {
       
-      console.log(totalPrice)
-      console.log(totalQuantity)
+      totalPrice +=parseInt(allProducts[index].price);
+      totalQuantity +=parseInt(allProducts[index].quantity)
+      // console.log(totalPrice)
+      // console.log(totalQuantity)
+
+      
    }
+   document.getElementById('totalPrice').innerHTML = totalPrice ;
+   document.getElementById('totalQuantity').innerHTML = totalQuantity;
+   divCart.classList.remove('d-none');
+   mainBody.classList.replace('mainbody','d-none')
+
+   
+   
+}
+
+function payment() {
+   let error = document.getElementById('error-msg');
+   let devCheckoutCart = document.getElementById('cartCheckout')
+   let PayStack = document.getElementById('paystack');
+   let fullName = document.forms["paymentForm"]["fName"].value;
+   let email = document.forms["paymentForm"]["email"].value;
+   let address = document.forms["paymentForm"]["address"].value
+   let alphabets =  /^[A-Za-z ]+$/;
+   let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+   if (fullName.trim() == '' || fullName.search(alphabets) == -1) {
+      error.classList.remove('d-none');
+      error.innerHTML = 'Please enter a valid name';
+   }else if (fullName.split(' ').length < 2){
+      error.classList.remove('d-none');
+      error.innerHTML = 'Please enter a valid name';
+   } else if (email.trim() == '' || email.search(re) == -1) {
+      error.classList.remove('d-none');
+      error.innerHTML = 'Please enter a valid email';
+   } else if (address.trim() == ''){
+      error.classList.remove('d-none');
+      error.innerHTML = 'Please enter an email';
+   }else {
+      PayStack.classList.remove('d-none')
+      devCheckoutCart.classList.add('d-none');
+      document.getElementById('clientName'). innerHTML = fullName;
+      document.getElementById('clientEmail'). innerHTML = email;
+      document.getElementById('amount'). innerHTML = totalPrice;
+     
+   }
+
+
+ 
+   return false ;
+}
+
+// paystack //
+
+
+var paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener('submit', payWithPaystack, false);
+function payWithPaystack() {
+  let handler = PaystackPop.setup({
+    key: 'pk_test_b4ef67ba382c06d51a7f34ccf5108776fc17625c', // Replace with your public key
+    email: document.getElementById("clientEmail").value,
+    amount: document.getElementById("amount").value * 100,
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+  handler.openIframe();
+}
+
    
   
